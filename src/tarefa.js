8a2a5b8a2f292b447139ae7a694f863d10a6ed61
Matheus Barbosa <matts.barbosa@gmail.com>
2023-05-24 16:27:23 -0300

    initial commit

---
+++ src/tarefa.js
@@ -0,0 +1,9 @@
+//funcao multiplicação e saudação
+function multiplica(n1, n2) {
+    return n1 * n2;
+}
+function dizOla(nome) {
+    return "".concat(nome, " diz ola");
+}
+console.log(dizOla('matheus'));
+console.log(multiplica(10, 26));
---
+++ src/tarefa.ts
@@ -0,0 +1,12 @@
+//funcao multiplicação e saudação
+
+function multiplica(n1: number, n2: number): number {
+    return n1 * n2;
+}
+
+function dizOla(nome: string): string {
+    return `${nome} diz ola`;
+}
+
+console.log(dizOla('matheus'));
+console.log(multiplica(10,26));
